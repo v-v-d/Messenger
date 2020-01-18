316298c728408f88602488f9808b513df1b06bdf
v-v-d <v-v-d@bk.ru>
2020-01-18 03:57:41 +0300

    client side main script linter corrections done

--- client_app.py
+++ client_app.py
@@ -4,13 +4,13 @@ import sys
 from client import Client
 
 
-server_address = None
-server_port = None
+SERVER_ADDRESS = None
+SERVER_PORT = None
 
 try:
-    server_address = sys.argv[1]
-    server_port = int(sys.argv[2])
-    if not 1024 < server_port < 65535:
+    SERVER_ADDRESS = sys.argv[1]
+    SERVER_PORT = int(sys.argv[2])
+    if not 1024 < SERVER_PORT < 65535:
         raise ValueError
 except IndexError:
     print('Server host and port will be set by default.')
@@ -18,9 +18,9 @@ except ValueError:
     print('Port number must be in range 1024 - 65535.')
     sys.exit(1)
 
-if server_address and server_port:
-    client = Client(host=server_address, port=server_port)
+if SERVER_ADDRESS and SERVER_PORT:
+    CLIENT = Client(host=SERVER_ADDRESS, port=SERVER_PORT)
 else:
-    client = Client()
+    CLIENT = Client()
 
-client.run()
+CLIENT.run()
