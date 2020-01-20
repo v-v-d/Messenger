3a3ba7f8669825716c66d087cd320a782fed3c7c
v-v-d <v-v-d@bk.ru>
2020-01-20 15:44:02 +0300

    server side args parsing function refactoring

--- server/__main__.py
+++ server/__main__.py
@@ -2,12 +2,12 @@
 # Via terminal run 'python server' in root dir if you don't want to set up address and port.
 # It will be set by default. Otherwise run 'python server -a 0.0.0.0 -p 8888' for example for set it up.
 from app import Server
-from utils import get_socket_parameters
+from utils import get_socket_params
 
 
 try:
-    SOCKET_PARAMETERS = get_socket_parameters()
-    SERVER = Server(host=SOCKET_PARAMETERS.address, port=SOCKET_PARAMETERS.port)
+    SOCKET_PARAMS = get_socket_params()
+    SERVER = Server(host=SOCKET_PARAMS.address, port=SOCKET_PARAMS.port)
     SERVER.run()
 except ValueError as error:
     print(error)
