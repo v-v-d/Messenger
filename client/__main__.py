e307e879d286ded0836b33c03277f6208b45f30c
v-v-d <v-v-d@bk.ru>
2020-01-20 03:41:12 +0300

    client side args parsing updated

--- client/__main__.py
+++ client/__main__.py
@@ -1,18 +1,11 @@
 """Messenger client side main script."""
+# Via terminal run 'python client' in root dir if you don't want to set up address and port.
+# It will be set by default. Otherwise run 'python client -a 0.0.0.0 -p 8888' for example for set it up.
 from app import Client
 from utils import get_socket_parameters
 
 
-# Run the file via command line with 'python .' if you don't want to set up address and port.
-# It will be set by default. Otherwise run 'python . -a 0.0.0.0 -p 8888' for example for set up it.
 SOCKET_PARAMETERS = get_socket_parameters()
-ADDRESS = SOCKET_PARAMETERS['address']
-PORT = SOCKET_PARAMETERS['port']
 
-if ADDRESS and PORT:
-    CLIENT = Client(host=ADDRESS, port=PORT)
-else:
-    print('Client host and server port will be set by default.')
-    CLIENT = Client()
-
+CLIENT = Client(host=SOCKET_PARAMETERS.address, port=SOCKET_PARAMETERS.port)
 CLIENT.run()
--- client/utils.py
+++ client/utils.py
@@ -1,28 +1,19 @@
 """Client side utility functions for Messenger app."""
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 
 
 def get_socket_parameters():
-    """Get the server IP address and server listening port from command line."""
-    socket_parameters = {
-        'address': None,
-        'port': None,
-    }
-
-    parser = ArgumentParser(description='Set the server IP address and server listening port.')
+    """
+    Get the client IP address and server listening port from command line.
+    Address = 'localhost' and port = 7777 set by default.
+    """
+    parser = ArgumentParser(description='Set the client IP address and server listening port.')
     parser.add_argument(
-        '-a', '--address', type=str,
-        required=False, help='Set server IP address'
+        '-a', '--address', type=str, default='0.0.0.0',
+        required=False, help='Set client IP address'
     )
     parser.add_argument(
-        '-p', '--port', type=int,
+        '-p', '--port', type=int, default=7777,
         required=False, help='Set server listening port'
     )
-    args = parser.parse_args()
-
-    if args.address:
-        socket_parameters['address'] = args.address
-    if args.port:
-        socket_parameters['port'] = args.port
-
-    return socket_parameters
+    return parser.parse_args()
