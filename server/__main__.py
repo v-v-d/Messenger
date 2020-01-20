8ecd0b0c8f0bd0a30d80d1b7a8edb8200ef175c9
v-v-d <v-v-d@bk.ru>
2020-01-20 03:42:51 +0300

    server side comment mistake fixed

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,6 @@
 """Messenger server side main script."""
-# Via terminal run 'python client' in root dir if you don't want to set up address and port.
-# It will be set by default. Otherwise run 'python client -a 0.0.0.0 -p 8888' for example for set it up.
+# Via terminal run 'python server' in root dir if you don't want to set up address and port.
+# It will be set by default. Otherwise run 'python server -a 0.0.0.0 -p 8888' for example for set it up.
 from app import Server
 from utils import get_socket_parameters
 
