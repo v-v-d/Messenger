f510cee9120497daf37e4e0d750b1a9f0737fd3e
v-v-d <v-v-d@bk.ru>
2020-01-20 15:43:43 +0300

    client side args parsing function refactoring

--- client/__main__.py
+++ client/__main__.py
@@ -2,12 +2,12 @@
 # Via terminal run 'python client' in root dir if you don't want to set up address and port.
 # It will be set by default. Otherwise run 'python client -a 0.0.0.0 -p 8888' for example for set it up.
 from app import Client
-from utils import get_socket_parameters
+from utils import get_socket_params
 
 
 try:
-    SOCKET_PARAMETERS = get_socket_parameters()
-    CLIENT = Client(host=SOCKET_PARAMETERS.address, port=SOCKET_PARAMETERS.port)
+    SOCKET_PARAMS = get_socket_params()
+    CLIENT = Client(host=SOCKET_PARAMS.address, port=SOCKET_PARAMS.port)
     CLIENT.run()
 except ValueError as error:
     print(error)
--- client/utils.py
+++ client/utils.py
@@ -2,10 +2,11 @@
 from argparse import ArgumentParser
 
 
-def get_socket_parameters():
+def parse_args(*args):
     """
-    Get the client IP address and server listening port from command line.
-    Address = 'localhost' and port = 7777 set by default.
+    Create a command line argument parser and add arguments to it.
+    :param (Tuple) args: Tuple with arguments for parser.
+    :return (argparse.Namespace): Namespace with added arguments.
     """
     parser = ArgumentParser(description='Set the client IP address and server listening port.')
     parser.add_argument(
@@ -16,9 +17,27 @@ def get_socket_parameters():
         '-p', '--port', type=int, default=7777,
         required=False, help='Set server listening port'
     )
-    args = parser.parse_args()
-    if not 1024 <= args.port <= 65536:
-        raise ValueError(f'ValueError: port must be 1024-65535, {args.port} given')
-    return args
+    return parser.parse_args(*args)
 
 
+def get_socket_params(*args):
+    """
+    Get the client IP address and server listening port from command line.
+    Address = 'localhost' and port = 7777 is set by default.
+    :param (Tuple) args: Tuple with arguments for command line parser.
+    :return (argparse.Namespace): Namespace with socket parameters if port is valid, raise ValueError otherwise.
+    """
+    parser = parse_args(*args)
+    if is_port_valid(parser.port):
+        return parser
+    else:
+        raise ValueError(f'ValueError: port must be 1024-65535, {parser.port} given.')
+
+
+def is_port_valid(port):
+    """
+    Validate port.
+    :param (int) port: Server listening port.
+    :return (bool): True if valid, False otherwise.
+    """
+    return 1024 <= port <= 65536
