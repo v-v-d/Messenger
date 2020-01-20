962da1f7de4c5d81925e9067d88b048d047481ca
v-v-d <v-v-d@bk.ru>
2020-01-20 03:41:38 +0300

    server side args parsing updated

--- server/utils.py
+++ server/utils.py
@@ -3,26 +3,17 @@ from argparse import ArgumentParser
 
 
 def get_socket_parameters():
-    """Get the server IP address and server listening port from command line."""
-    socket_parameters = {
-        'address': None,
-        'port': None,
-    }
-
+    """
+    Get the server IP address and server listening port from command line.
+    Address = '0.0.0.0' and port = 7777 set by default.
+    """
     parser = ArgumentParser(description='Set the server IP address and server listening port.')
     parser.add_argument(
-        '-a', '--address', type=str,
+        '-a', '--address', type=str, default='0.0.0.0',
         required=False, help='Set server IP address'
     )
     parser.add_argument(
-        '-p', '--port', type=int,
+        '-p', '--port', type=int, default=7777,
         required=False, help='Set server listening port'
     )
-    args = parser.parse_args()
-
-    if args.address:
-        socket_parameters['address'] = args.address
-    if args.port:
-        socket_parameters['port'] = args.port
-
-    return socket_parameters
+    return parser.parse_args()
