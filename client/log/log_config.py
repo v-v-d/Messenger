7d0712ad60eccfede1b3acf595c99a3113f55528
v-v-d <v-v-d@bk.ru>
2020-01-25 05:13:06 +0300

    client side log config added to client/log

---
+++ client/log/log_config.py
@@ -0,0 +1,37 @@
+"""Basic configuration for the client logging system."""
+import os
+
+
+FILENAME = 'client.log'
+CURRENT_PATH = os.path.dirname(os.path.relpath(__file__))
+FILE_PATH = os.path.join(CURRENT_PATH, FILENAME)
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'default',
+            'filename': FILE_PATH,
+            'encoding': 'UTF-8',
+        },
+    },
+    'loggers': {
+        'client': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    },
+}
