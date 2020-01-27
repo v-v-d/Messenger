6a5638e7f135360f46bb33ebcbcf2e2fd421549f
v-v-d <v-v-d@bk.ru>
2020-01-28 00:15:56 +0300

    client side main script logging added

--- client/__main__.py
+++ client/__main__.py
@@ -1,13 +1,20 @@
 """Messenger client side main script."""
 # Via terminal run 'python client' in root dir if you don't want to set up address and port.
 # It will be set by default. Otherwise run 'python client -a 0.0.0.0 -p 8888' for example for set it up.
+from logging import getLogger
+from logging.config import dictConfig
+
 from app import Client
 from utils import get_socket_params
+from log.log_config import LOGGING
 
+
+dictConfig(LOGGING)
+LOGGER = getLogger('client')
 
 try:
     SOCKET_PARAMS = get_socket_params()
     CLIENT = Client(host=SOCKET_PARAMS.address, port=SOCKET_PARAMS.port)
     CLIENT.run()
 except ValueError as error:
-    print(error)
+    LOGGER.error(f'{error}.')
