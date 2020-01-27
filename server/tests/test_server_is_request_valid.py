9f0393bfc251478d1d2d045bd30296500a3418d6
v-v-d <v-v-d@bk.ru>
2020-01-28 00:22:20 +0300

    server side tests for Server.is_request_valid method added

---
+++ server/tests/test_server_is_request_valid.py
@@ -0,0 +1,38 @@
+"""Tests for client is_response_valid Client class method."""
+from time import time
+
+import pytest
+
+from app import Server
+
+
+@pytest.fixture
+def valid_request_fixture():
+    """Valid request fixture.
+    :return (dict): Dict with valid request.
+    """
+    return {
+        'action': 'presence',
+        'time': time(),
+        'user': {
+            'account_name': 'Guest'
+        }
+    }
+
+
+@pytest.fixture
+def invalid_request_fixture():
+    """Valid request fixture.
+    :return (dict): Dict with invalid request.
+    """
+    return {}
+
+
+def test_valid_is_request_valid(valid_request_fixture):
+    """Test that method returns True when valid request passed."""
+    assert Server.is_request_valid(valid_request_fixture)
+
+
+def test_invalid_is_response_valid(invalid_request_fixture):
+    """Test that method raises ValueError when invalid request passed."""
+    assert not Server.is_request_valid(invalid_request_fixture)
