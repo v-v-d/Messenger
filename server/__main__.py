fa83454d143b23809abda01ff544d2854b349e1f
v-v-d <v-v-d@bk.ru>
2020-01-31 15:27:35 +0300

    run server via context manager

--- server/__main__.py
+++ server/__main__.py
@@ -1,6 +1,7 @@
 """Messenger server side main script."""
-# Via terminal run 'python server' in root dir if you don't want to set up address and port.
-# It will be set by default. Otherwise run 'python server -a 0.0.0.0 -p 8888' for example for set it up.
+# Via terminal run 'python server' in root dir if you don't want to
+# set up address and port. It will be set by default. Otherwise run
+# 'python server -a 0.0.0.0 -p 8888' for example for set it up.
 from logging import getLogger
 from logging.config import dictConfig
 
@@ -14,7 +15,9 @@ LOGGER = getLogger('server')
 
 try:
     SOCKET_PARAMS = get_socket_params()
-    SERVER = Server(host=SOCKET_PARAMS.address, port=SOCKET_PARAMS.port)
-    SERVER.run()
+
+    with Server(host=SOCKET_PARAMS.address, port=SOCKET_PARAMS.port) as server:
+        server.run()
+
 except ValueError as error:
     LOGGER.error(f'{error}.')
