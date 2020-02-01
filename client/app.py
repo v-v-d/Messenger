3f0b919efdd3090c848365a5a1d4bdef181a11e7
v-v-d <v-v-d@bk.ru>
2020-02-02 01:02:10 +0300

    client side main class updated for using app via context manager; def get_request updated for making request with ./client/protocol.py; handling --mode and --name args added.

--- client/app.py
+++ client/app.py
@@ -1,19 +1,18 @@
 """Client side Messenger app."""
-import sys
 import json
 from zlib import compress, decompress
 from logging import getLogger
 from logging.config import dictConfig
-from time import time
 from socket import socket, AF_INET, SOCK_STREAM
 
 from log.log_config import LOGGING
+from protocol import make_request
 from decorators import log
 
 
 class Client:
     """Messenger client side main class."""
-    def __init__(self, host='127.0.0.1', port=7777, buffersize=1024, name='Guest'):
+    def __init__(self, host='127.0.0.1', port=7777, buffersize=1024, name='Guest', mode='recv'):
         """
         Client initialization.
         :param (str) host: Client IP address.
@@ -25,18 +24,34 @@ class Client:
         self.host = host
         self.port = port
         self.name = name
-        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket = None
 
         dictConfig(LOGGING)
         self.logger = getLogger('client')
 
+        self.mode = mode
+
+    def __enter__(self):
+        if not self.socket:
+            self.socket = socket(AF_INET, SOCK_STREAM)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        message = 'Client shutdown.'
+        if exc_type and exc_type is not KeyboardInterrupt:
+            message = f'Client stopped with error: {exc_val}'
+        self.logger.info(message)
+        return True
+
     def run(self):
         """Run the client."""
         self.connect()
-        self.write()
-        self.read()
-        self.socket.close()
-        self.logger.info('Client shutdown.')
+
+        if self.mode == 'send':
+            self.write()
+
+        elif self.mode == 'recv':
+            self.read()
 
     def connect(self):
         """Connect to server with host and port attributes."""
@@ -45,7 +60,6 @@ class Client:
             self.logger.info(f'Client connected to server with {self.host}:{self.port}.')
         except ConnectionRefusedError as error:
             self.logger.critical(f'Connection closed. Error: {error}.')
-            sys.exit(1)
 
     @log
     def write(self):
@@ -59,13 +73,9 @@ class Client:
         """Get request to server.
         :return (dict): Dict with request body.
         """
-        return {
-            'action': 'presence',
-            'time': time(),
-            'user': {
-                'account_name': self.name
-            }
-        }
+        action = 'presense'
+        data = ''
+        return make_request(action, data, self.name)
 
     @log
     def read(self):
