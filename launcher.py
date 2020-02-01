7fb60e29a50c06a508da91122f7c6b70842aefac
v-v-d <v-v-d@bk.ru>
2020-02-02 01:02:58 +0300

    handling argument --name added to launcher

--- launcher.py
+++ launcher.py
@@ -14,8 +14,8 @@ while True:
     elif ACTION == 's':
         PROCESS.append(Popen('python __main__.py', creationflags=CREATE_NEW_CONSOLE))
 
-        for _ in range(2):
-            PROCESS.append(Popen('python client -m send', creationflags=CREATE_NEW_CONSOLE))
+        for i in range(1, 3):
+            PROCESS.append(Popen(f'python client -n user_{i} -m send', creationflags=CREATE_NEW_CONSOLE))
 
         for _ in range(3):
             PROCESS.append(Popen('python client', creationflags=CREATE_NEW_CONSOLE))
