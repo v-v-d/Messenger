f953f75a499154f4c338460ed19718f8fe6e15e7
v-v-d <v-v-d@bk.ru>
2020-02-03 02:28:01 +0300

    client side main class arg --mode handling removed, threading added

--- client/app.py
+++ client/app.py
@@ -1,9 +1,11 @@
-"""Client side Messenger app."""
+"""Client side of Messenger app."""
 import json
-from zlib import compress, decompress
+import zlib
+from time import sleep
 from logging import getLogger
 from logging.config import dictConfig
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread, enumerate
 
 from log.log_config import LOGGING
 from protocol import is_response_valid, make_request
@@ -12,15 +14,15 @@ from decorators import log
 
 class Client:
     """Messenger client side main class."""
-    def __init__(self, host='127.0.0.1', port=7777, buffersize=1024, name='Guest', mode='recv'):
+    def __init__(self, host='127.0.0.1', port=7777, bufsize=1024, name='Guest'):
         """
         Client initialization.
         :param (str) host: Client IP address.
         :param (int) port: Server listening port.
-        :param (int) buffersize: TCP max data size in bytes.
+        :param (int) bufsize: The maximum amount of data to be received at once.
         :param (str) name: Username.
         """
-        self.buffersize = buffersize
+        self.bufsize = bufsize
         self.host = host
         self.port = port
         self.name = name
@@ -28,8 +30,6 @@ class Client:
 
         dictConfig(LOGGING)
         self.logger = getLogger('client')
-
-        self.mode = mode
 
     def __enter__(self):
         if not self.socket:
@@ -46,13 +46,11 @@ class Client:
     def run(self):
         """Run the client."""
         self.connect()
+        self.write()
+        self.read()
+        self.check_threads_health()
 
-        if self.mode == 'send':
-            self.write()
-
-        elif self.mode == 'recv':
-            self.read()
-
+    @log
     def connect(self):
         """Connect to server with host and port attributes."""
         try:
@@ -61,13 +59,22 @@ class Client:
         except (ConnectionResetError, ConnectionError, ConnectionAbortedError) as error:
             self.logger.critical(f'Connection closed. Error: {error}.')
 
-    @log
     def write(self):
-        """Send bytes request to server."""
-        request = self.get_request()
-        bytes_request = json.dumps(request).encode('UTF-8')
-        self.socket.send(compress(bytes_request))
-        self.logger.debug(f'Client send request {request}.')
+        """Start writer thread for sending requests to server."""
+        Thread(target=self.start_writing, daemon=True, name='writer').start()
+
+    def start_writing(self):
+        """Send compressed bytes request to server."""
+        while True:
+            try:
+                request = self.get_request()
+                bytes_request = json.dumps(request).encode('UTF-8')
+                self.socket.send(zlib.compress(bytes_request))
+                self.logger.debug(f'Client send request {request}.')
+
+            except Exception as error:
+                self.logger.critical(f'Can\'t send request. Error: {error}')
+                break
 
     def get_request(self):
         """Get request to server.
@@ -77,23 +84,58 @@ class Client:
         data = input('data: ')
         return make_request(action, data, self.name)
 
-    @log
     def read(self):
-        """Read response from server."""
-        try:
-            response = self.get_response()
-            self.logger.debug(f'Client got response: {response}.')
+        """Start reader thread for reading responses from server."""
+        Thread(target=self.start_reading, daemon=True, name='reader').start()
 
-            data = response.get('data')
-            print(f'{data.get("user")}: {data.get("text")}')
-        except (ValueError, json.JSONDecodeError):
-            self.logger.critical('Failed to decode server response.')
+    def start_reading(self):
+        """Read response from server."""
+        while True:
+            try:
+                response = self.get_response()
+                self.logger.debug(f'Client got response: {response}.')
+                self.print_message(response)
+
+            except (ValueError, json.JSONDecodeError):
+                self.logger.critical('Failed to decode server response.')
+
+            except Exception as error:
+                self.logger.critical(f'Can\'t read response. Error: {error}')
+                break
 
     def get_response(self):
-        """Get decoded response from server.
+        """Get decompressed and decoded response from server.
         :return (dict): Dict with response body.
         """
-        bytes_response = decompress(self.socket.recv(self.buffersize))
+        bytes_response = zlib.decompress(self.socket.recv(self.bufsize))
         response = json.loads(bytes_response.decode('UTF-8'))
         if is_response_valid(response):
             return response
+
+    @staticmethod
+    def print_message(response):
+        """
+        Print message from user or error message from server.
+        :param (dict) response: Dict with response body.
+        """
+        data = response.get('data')
+        code = response.get('code')
+
+        if isinstance(data, dict):
+            message = f'\n{data.get("user")}: {data.get("text")}'
+        else:
+            message = f'\n{code}: {data}'
+
+        print(message)
+
+    @staticmethod
+    def check_threads_health():
+        """Reader and writer threads health checking."""
+        current_th_names = [thread.name for thread in enumerate()]
+        th_names = ('reader', 'writer')
+
+        while True:
+            sleep(1)
+            if all(th_name in current_th_names for th_name in th_names):
+                continue
+            break
