e578b81f10f37357654b7a12318a7967c9baf8b3
v-v-d <v-v-d@bk.ru>
2020-02-16 04:20:56 +0300

    client side utils port validation logic moved to descriptor

--- client/utils.py
+++ client/utils.py
@@ -10,7 +10,7 @@ def parse_args(*args):
     """
     parser = ArgumentParser(description='Set the client parameters')
     parser.add_argument(
-        '-a', '--address', type=str, default='localhost',
+        '-a', '--address', type=str, default='127.0.0.1',
         required=False, help='Set client IP address'
     )
     parser.add_argument(
@@ -22,27 +22,3 @@ def parse_args(*args):
         required=False, help='Set username'
     )
     return parser.parse_args(*args)
-
-
-def get_valid_parser(*args):
-    """
-    Get the client IP address and server listening port from command line.
-    Address = 'localhost' and port = 7777 is set by default.
-    :param (Tuple) args: Tuple with arguments for command line parser.
-    :return (argparse.Namespace): Namespace with socket parameters if port is valid, raise ValueError otherwise.
-    """
-    parser = parse_args(*args)
-    if is_parser_valid(parser):
-        return parser
-    raise ValueError(f'Error: port must be 1024-65536, {parser.port} given.')
-
-
-def is_parser_valid(parser):
-    """
-    Validate parser.
-    :param (argparse.Namespace) parser: Parser namespace.
-    :return (bool): True if valid, False otherwise.
-    """
-    min_port_value = 1024
-    max_port_value = 65536
-    return min_port_value <= parser.port <= max_port_value
