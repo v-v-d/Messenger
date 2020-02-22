ef940052549d9b1c2d719f1ad7dec1c7e7665c8e
v-v-d <v-v-d@bk.ru>
2020-02-22 17:21:25 +0300

    client side protocol changed

--- client/protocol.py
+++ client/protocol.py
@@ -8,24 +8,36 @@ def is_response_valid(response):
     :param (dict) response: Dict from server with response body.
     :return (bool) : True if all valid keys in response, False otherwise.
     """
-    valid_keys = ('action', 'time', 'data', 'code')
+    valid_keys = ('action', 'time', 'data', 'code', 'address')
     if all(key in response for key in valid_keys):
         return True
     raise ValueError
 
 
-def make_request(action, data, user):
+def make_request(action, data, r_addr, l_addr, token=None):
     """
     Make request based on passed arguments and timestamp.
     :param (str) action: Protocol specially reserved action.
     The server will find a special way for response based on it.
-    :param (dict) data: Transmitted user data (message).
-    :param (str) user: User who make the request.
+    :param (str) data: Transmitted user data (message).
+    :param (namedtuple) r_addr: Remote client address.
+    :param (namedtuple) l_addr: Local client address.
+    :param (str) token: Client session token.
     :return (dict) : Dict with request body.
     """
     return {
         'action': action,
         'time': time(),
         'data': data,
-        'user': user,
+        'token': token,
+        'address': {
+            'remote': {
+                'addr': r_addr.addr,
+                'port': r_addr.port,
+            },
+            'local': {
+                'addr': l_addr.addr,
+                'port': l_addr.port,
+            },
+        },
     }
