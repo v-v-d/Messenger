dbb2c2fcc5684c2b36702e154d86db3ef5d3dfe4
v-v-d <v-v-d@bk.ru>
2020-02-22 17:25:33 +0300

    server side utils functions added to ./server/db/utils.py

--- server/db/utils.py
+++ server/db/utils.py
@@ -1,27 +1,53 @@
-"""Server side database utilities for Messenger app."""
-from contextlib import contextmanager
+import hashlib
 
-from sqlalchemy.orm import sessionmaker
-
-from db.models import ENGINE, Base
-
-Session = sessionmaker(bind=ENGINE)
+from db.database import session_scope
+from db.models import ClientSession, Client
+from db.settings import SECRET_KEY
 
 
-def migrate_db():
-    """Propagate changes you make to your models into your database schema."""
-    Base.metadata.create_all(ENGINE)
+def get_validation_errors(request, *attrs):
+    errs = {}
+    [errs.update({attr: 'Attribute is required'}) for attr in attrs if attr not in request]
+    return errs
 
 
-@contextmanager
-def session_scope():
-    """Database connection context manager."""
-    session = Session()
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+def get_message(request, messages):
+    request_data = request.get('data')
+    message_id = request_data.get('message_id')
+    for msg in messages:    # TODO: Оптимизировать поиск
+        if msg.id == message_id:
+            return msg
+
+
+def authenticate(login, password):
+    with session_scope() as session:
+        client = session.query(Client).filter_by(name=login).first()
+
+        if client and password == client.password:
+            for client_session in client.sessions:
+                if not client_session.closed:
+                    raise ValueError
+            return client
+
+
+def login(request, client):
+    with session_scope() as session:
+        hash_obj = hashlib.sha256()
+        hash_obj.update(SECRET_KEY.encode('UTF-8'))
+        hash_obj.update(str(request.get('time')).encode('UTF-8'))
+        token = hash_obj.hexdigest()
+
+        socket_info = request.get('address')
+        remote_socket_info = socket_info.get('remote')
+        local_socket_info = socket_info.get('local')
+
+        client_session = ClientSession(
+            client=client,
+            token=token,
+            remote_addr=remote_socket_info.get('addr'),
+            remote_port=remote_socket_info.get('port'),
+            local_addr=local_socket_info.get('addr'),
+            local_port=local_socket_info.get('port')
+        )
+        session.add(client_session)
+        return token
