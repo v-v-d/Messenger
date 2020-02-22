18e787f7404b8103923e743d6386b89435c5c893
v-v-d <v-v-d@bk.ru>
2020-02-22 17:23:59 +0300

    server side models changed. Relationships added to models

--- server/db/models.py
+++ server/db/models.py
@@ -1,7 +1,9 @@
 """Server side database models for Messenger app."""
+from datetime import datetime
+
 from sqlalchemy import (
-    create_engine, MetaData,
-    Column, Integer, String, DateTime, ForeignKey
+    create_engine, MetaData, Column,
+    Integer, String, DateTime, ForeignKey, Boolean
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -12,31 +14,65 @@ ENGINE = create_engine(DB_CONNECTION_URL, echo=False, pool_recycle=7200)
 Base = declarative_base(metadata=MetaData(bind=ENGINE))
 
 
-class Users(Base):
-    """Users model."""
-    __tablename__ = 'users'
+class Client(Base):
+    """Client model."""
+    __tablename__ = 'clients'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(64), unique=True, nullable=False)
-    password = Column(String(64), nullable=False)
+    name = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    sent_messages = relationship('Message', foreign_keys='Message.from_client_id', back_populates='from_client')
+    gotten_messages = relationship('Message', foreign_keys='Message.to_client_id', back_populates='to_client')
+    sessions = relationship('ClientSession', back_populates='client')
+    friends = relationship('ClientContact', foreign_keys='ClientContact.owner_id', back_populates='owner_contact')
+    owners = relationship('ClientContact', foreign_keys='ClientContact.friend_id', back_populates='friend_contact')
+
+    def __repr__(self):
+        return self.name
 
 
-class ActiveUsers(Base):
-    """Active users model."""
-    __tablename__ = 'active_users'
+class Message(Base):
+    """Message model."""
+    __tablename__ = 'messages'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    addr = Column(String(64), nullable=False)
-    port = Column(Integer, nullable=False)
-    login_time = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('Users', back_populates='active_users')
+    text = Column(String, nullable=False)
+    created = Column(DateTime, nullable=False)
+    from_client_id = Column(Integer, ForeignKey('clients.id'))
+    to_client_id = Column(Integer, ForeignKey('clients.id'))
+    from_client = relationship('Client', foreign_keys=[from_client_id], back_populates='sent_messages')
+    to_client = relationship('Client', foreign_keys=[to_client_id], back_populates='gotten_messages')
+
+    def __repr__(self):
+        return self.text
 
 
-class LoginHistory(Base):
-    """Login history model."""
-    __tablename__ = 'login_history'
+class ClientSession(Base):
+    """Client session model."""
+    __tablename__ = 'sessions'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    addr = Column(String(64), nullable=False)
-    port = Column(Integer, nullable=False)
-    date = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('Users', back_populates='login_history')
+    token = Column(String, nullable=False, unique=True)
+    created = Column(DateTime, default=datetime.now())
+    closed = Column(DateTime, nullable=True)
+    remote_addr = Column(String, nullable=False)
+    remote_port = Column(Integer, nullable=False)
+    local_addr = Column(String, nullable=False)
+    local_port = Column(Integer, nullable=False)
+    client_id = Column(Integer, ForeignKey('clients.id'))
+    client = relationship('Client', back_populates='sessions')
+
+
+class ClientContact(Base):
+    """Client contact model."""
+    __tablename__ = 'client_contacts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    confirmed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('clients.id'))
+    friend_id = Column(Integer, ForeignKey('clients.id'))
+    owner_contact = relationship('Client', foreign_keys=[owner_id], back_populates='friends')
+    friend_contact = relationship('Client', foreign_keys=[friend_id], back_populates='owners')
+
+    def __repr__(self):
+        return f'{self.owner_contact} contact list'
