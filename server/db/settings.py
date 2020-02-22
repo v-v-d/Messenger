398748de8c428cda0ddf4eebcf8fcc656a2e3077
v-v-d <v-v-d@bk.ru>
2020-02-22 17:24:47 +0300

    server side SECRET_KEY added to ./server/db/settings.py

--- server/db/settings.py
+++ server/db/settings.py
@@ -1,6 +1,12 @@
 """Server side database settings for Messenger app."""
 import os
 
+import settings
+
 BASE_DIR = os.path.dirname(__file__)
 
 DB_CONNECTION_URL = f'sqlite:///{BASE_DIR}/default.db'
+
+DEFAULT_SECRET_KEY = 'zp5bK7Ah'
+
+SECRET_KEY = getattr(settings, 'SECRET_KEY', DEFAULT_SECRET_KEY)
