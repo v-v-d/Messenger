9af13b9ee077c5565e8748e980529f49e872a883
v-v-d <v-v-d@bk.ru>
2020-02-23 04:52:34 +0300

    launcher updated

--- launcher.py
+++ launcher.py
@@ -1,23 +1,55 @@
 """Launcher for running multiple consoles."""
 from subprocess import Popen, CREATE_NEW_CONSOLE
 
-PROCESS = []
+
+PROCESSES = []
+
+
+def print_help():
+    print('h - help')
+    print('q - quit')
+    print('s - run server')
+    print('k - run clients')
+    print('x - close all windows')
+
+
+def quit_from_launcher():
+    return True
+
+
+def start_server():
+    PROCESSES.append(Popen(['py', '-3', 'server', '-m'], creationflags=CREATE_NEW_CONSOLE))
+
+
+def start_clients():
+    clients_qty = int(input('How many clients you want to start: '))
+
+    for i in range(clients_qty):
+        PROCESSES.append(Popen(f'python client -n user_{i}', creationflags=CREATE_NEW_CONSOLE))
+
+
+def kill_processes():
+    while PROCESSES:
+        victim = PROCESSES.pop()
+        victim.kill()
+
+
+MENU = {
+    'h': print_help,
+    'q': quit_from_launcher,
+    's': start_server,
+    'k': start_clients,
+    'x': kill_processes,
+}
+
+print('Enter action. "h" for help.')
 
 while True:
-    ACTION = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, '
-                   'x - закрыть все окна: ')
+    ACTION = input()
 
-    if ACTION == 'q':
-        break
-
-    elif ACTION == 's':
-        PROCESS.append(Popen(['python', 'server', '-m'], creationflags=CREATE_NEW_CONSOLE))
-
-        for i in range(1, 4):
-            PROCESS.append(Popen(f'python client -n user_{i}', creationflags=CREATE_NEW_CONSOLE))
-
-    elif ACTION == 'x':
-        while PROCESS:
-            victim = PROCESS.pop()
-            victim.kill()
+    if MENU.get(ACTION):
+        IS_CLOSED = MENU[ACTION]()
+        if IS_CLOSED:
+            break
+    else:
+        print('Action not supported. Enter "h" for help.')
