446d7130b0632a4bd95ea9bde863b94090c0a730
v-v-d <v-v-d@bk.ru>
2020-03-17 03:18:29 +0300

    client side - db logic added to main class and main script

--- client/__main__.py
+++ client/__main__.py
@@ -3,23 +3,28 @@ from logging import getLogger
 from logging.config import dictConfig
 
 from app import Client
-from db.database import migrate_db
-from utils import PARSER
+from db.clients_db import migrate_db
+from db.utils import get_active_client_name_from_clients_db
 from log.log_config import LOGGING
-
+from ui.app import GUIApplication
+from utils import PARSER
 
 dictConfig(LOGGING)
 LOGGER = getLogger('client')
 
-if PARSER.migrate:
-    migrate_db()
-    LOGGER.debug('Migrations has been applied.')
+HOST, PORT = PARSER.address, PARSER.port
 
-HOST, PORT, NAME = PARSER.address, PARSER.port, PARSER.name
+migrate_db()
+
+NAME = get_active_client_name_from_clients_db()
 
 try:
-    with Client(host=HOST, port=PORT, name=NAME) as client:
-        client.run()
+    with Client(host=HOST, port=PORT, client_name=NAME) as client:
+        client.daemon = True
+        client.start()
+
+        with GUIApplication(client) as gui:
+            gui.render()
 
 except ValueError as error:
     LOGGER.critical(f'Can\'t run the client. Error: {error}')
--- client/app.py
+++ client/app.py
@@ -1,43 +1,45 @@
 """Client side of Messenger app."""
 import json
 import zlib
-from time import sleep
 from logging import getLogger
 from logging.config import dictConfig
 from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread, enumerate
+from threading import Thread
 
-from handlers import handle_response, handle_local_request
+from db.local_storage import LocalStorage
+from handlers import handle_protocol_object
 from log.log_config import LOGGING
 from protocol import is_response_valid, make_request
 from decorators import log
 from descriptors import PortValidator, HostValidator, BufsizeValidator
 from metaclasses import ClientVerifier
-from utils import make_presence_message, set_socket_info
+from resolvers import get_local_request_controller, get_response_controller
+from utils import make_presence_action_and_data, set_socket_info
 
 
-class Client(metaclass=ClientVerifier):
+class Client(Thread, metaclass=ClientVerifier):
     """Messenger client side main class."""
     host = HostValidator()
     port = PortValidator()
     bufsize = BufsizeValidator()
 
-    def __init__(self, host='127.0.0.1', port=7777, bufsize=1024, name='Guest'):
+    def __init__(self, host='127.0.0.1', port=7777, bufsize=1024, client_name=None):
         """
         Client initialization.
-        :param (str) host: Client IP address.
+        :param (str) host: Server IP address.
         :param (int) port: Server listening port.
         :param (int) bufsize: The maximum amount of data to be received at once.
-        :param (str) name: Username.
         """
+        super().__init__()
         self.bufsize = bufsize
         self.host = host
         self.port = port
-        self.name = name
+        self.client_name = client_name
         self.socket = None
-        self._token = None
+        self.token = None
         self._r_addr = None
         self._l_addr = None
+        self.database = None
 
         dictConfig(LOGGING)
         self.logger = getLogger('client')
@@ -45,12 +47,15 @@ class Client(metaclass=ClientVerifier):
     def __enter__(self):
         if not self.socket:
             self.socket = socket(AF_INET, SOCK_STREAM)
+        if self.client_name:
+            self.database = LocalStorage(self.client_name)
+            self.database.connect()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         message = 'Client shutdown.'
         if exc_type and exc_type is not KeyboardInterrupt:
-            message = f'Client stopped with error: {exc_type}: {exc_val}'
+            message = f'Client stopped with error: {exc_type}: {exc_val}.'
         self.logger.info(message)
         return True
 
@@ -58,8 +63,6 @@ class Client(metaclass=ClientVerifier):
         """Run the client."""
         self.connect()
         self.read()
-        self.write()
-        self.check_threads_health()
 
     @log
     def connect(self):
@@ -69,7 +72,7 @@ class Client(metaclass=ClientVerifier):
             self.logger.info(f'Client connected to server with {self.host}:{self.port}.')
 
             self._set_addresses()
-            make_presence_message(self.socket, self._r_addr, self._l_addr, self.name)
+            self.write(*make_presence_action_and_data(self.client_name))
         except (ConnectionResetError, ConnectionError, ConnectionAbortedError) as error:
             self.logger.critical(f'Connection closed. Error: {error}.')
 
@@ -77,38 +80,34 @@ class Client(metaclass=ClientVerifier):
         self._r_addr = set_socket_info(*self.socket.getsockname())
         self._l_addr = set_socket_info(self.host, self.port)
 
-    def write(self):
-        """Start writer thread for sending requests to server."""
-        Thread(target=self.start_writing, daemon=True, name='writer').start()
-
-    def start_writing(self):
+    def write(self, action, data):
         """Send compressed bytes request to server."""
-        while True:
-            try:
-                request = self.get_request()
+        try:
+            request = self.get_request(action, data)
+            local_response = handle_protocol_object(
+                request, get_local_request_controller, self.database
+            )
 
-                if not handle_local_request(request):
-                    bytes_request = json.dumps(request).encode('UTF-8')
-                    self.socket.send(zlib.compress(bytes_request))
-                    self.logger.debug(f'Client send request {request}.')
+            if not local_response:
+                bytes_request = json.dumps(request).encode('UTF-8')
+                self.socket.send(zlib.compress(bytes_request))
+                self.logger.debug(f'Client send request {request}.')
+            else:
+                return local_response
 
-            except Exception as error:
-                self.logger.critical(f'Can\'t send request. Error: {error}')
-                break
+        except Exception as error:
+            self.logger.critical(f'Can\'t send request. Error: {error}')
 
-    def get_request(self):
+    def get_request(self, action, data):
         """Get request to server.
         :return (dict): Dict with request body.
         """
-        action = input('enter action: ')
-        data = input('enter data: ')
-
         return make_request(
             action=action,
-            data=data,
+            data=json.dumps(data),
             r_addr=self._r_addr,
             l_addr=self._l_addr,
-            token=self._token
+            token=self.token
         )
 
     def read(self):
@@ -121,7 +120,7 @@ class Client(metaclass=ClientVerifier):
             try:
                 response = self.get_response()
                 self.logger.debug(f'Client got response: {response}.')
-                data = handle_response(response)
+                data = handle_protocol_object(response, get_response_controller, self.database)
                 if data:
                     self._set_token(data)
 
@@ -143,17 +142,5 @@ class Client(metaclass=ClientVerifier):
 
     def _set_token(self, data):
         token = data.get('token')
-        if token and not self._token:
-            self._token = token
-
-    @staticmethod
-    def check_threads_health():    # TODO: выглядит костыльно, переделать
-        """Reader and writer threads health checking."""
-        current_th_names = [thread.name for thread in enumerate()]
-        th_names = ('reader', 'writer')
-
-        while True:
-            sleep(1)    # TODO: выглядит костыльно, переделать
-            if all(th_name in current_th_names for th_name in th_names):
-                continue
-            break
+        if token and not self.token:
+            self.token = token
